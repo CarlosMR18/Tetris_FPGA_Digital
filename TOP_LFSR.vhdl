6400f818872f1575f23e56fb2801523388b5864a
CarlosMR18 <103529858+CarlosMR18@users.noreply.github.com>
2023-12-24 19:00:00 +0100

    Update TOP_LFSR.vhdl

--- TOP_LFSR.vhdl
+++ TOP_LFSR.vhdl
@@ -51,13 +51,15 @@ Port (  clk : in std_logic;
         lfsr_in : in std_logic_vector(4 downto 0);
         Generacion_pieza_flag : in std_logic;
         
+		TipoElegido: in std_logic;
+        SimpleElegida: in std_logic;
+		DobleElegida: in std_logic;
+		CuadradaElegida: in std_logic;
 		   
         Ena_Tipo : out std_logic;
         E_Tipo : out std_logic_vector(1 downto 0);
         E_Pos : out std_logic_vector(2 downto 0);
-        
-        PiezaElegida: in std_logic;
-        PosicionElegida: in std_logic;
+		MiPieza_TP_Nuevo : out unsigned(6 downto 0);
         Generacion_pieza_fin_flag : out std_logic;
         --Faltan salidas E()
 	);		   
@@ -76,23 +78,14 @@ architecture Behavioral of TOP_LFSR is
 		lfsr_out : out std_logic);
 	end component;
 	
-	type state_t is (Espera, Activo)
-	signal  STATE_LFSR : state_t;
-	
 	signal aux_pulso : std_logic_vector(2 downto 0);
 	signal  pulso_flag : std_logic;
 	
+	type state_t is (Espera, Generando_Tipo, Generando_Posicion)
+	signal  STATE_LFSR : state_t;
+	signal Reg_Tipo : std_logic_vector(1 downto 0);
+	
 Begin
-    entrada_flag : process(clk,reset)
-    begin
-        if(reset = '1') then
-            aux_pulso <= (others =>'0');
-        elsif(clk = '1' and clk'event) then
-            aux_pulso <= aux_pulso(1 downto 0)&Generacion_pieza_flag;
-        end if;
-    end process;
-    pulso_flag <= aux_pulso(1) and not(aux_pulso(2));
-    
     
     Maquina_Estados : process(clk,reset)
     begin
@@ -100,16 +93,47 @@ Begin
             STATE_LFSR <= Espera;
         elsif(clk = '1' and clk'event) then    
            case STATE_LFSR is
+		   
                when Espera =>
                    if(Generacion_pieza_flag = '1') then
-                    STATE_LFSR <= Activo_Tipo;
-               when Activo_Tipo =>
-                   if(PiezaElegida = '1') then
-                    STATE_LFSR <= Activo_Posicion;
+                    STATE_LFSR <= Generando_Tipo;
+					Reg_Tipo <= (others => '0');
+					
+               when Generando_Tipo =>
+                   if(TipoElegido = '1') then
+                    STATE_LFSR <= Generando_Posicion;
+					Reg_Tipo <= E_Tipo;
                     
-               when Activo_Posicion =>
-                   if(PosicionElegida = '1') then
+               when Generando_Posicion =>
+                   if(E_Tipo = "00" and SimpleElegida = '1' or E_Tipo = "01" and DobleElegida = '1' or E_Tipo = "10" and CuadradaElegida = '1') then
                     STATE_LFSR <= Espera;
+					case Reg_Tipo is
+						when "00" => 
+							if(SimpleElegida = '1') then
+								with E_Pos select
+									MiPieza_TP_Nuevo <=	"0000001" when "000",
+														"0001000" when "001",
+														"0010000" when "010",
+														"0100000" when "011",
+														"1000000" when "100",
+														"-------" when others;
+						when "01" => 
+							if(DobleElegida = '1') then
+								with E_Pos(1 downto 0) select
+									MiPieza_TP_Nuevo <=	"0100001" when "000",
+														"1100000" when "001",
+														"1010000" when "010",
+														"0011000" when "011",
+														"-------" when others;
+						when "10" => 
+							if(CuadradaElegida = '1') then
+								with E_Pos(0) select
+									MiPieza_TP_Nuevo <=	"0000000" when "0",
+														"0000000" when "1",
+														"-------" when others;
+						when "00" =>
+							--Nada
+														
              end case;       
         end if;
     end process;
@@ -132,4 +156,13 @@ Begin
                             reset -> reset,
                             lfsr_out -> lfsr_in(4)):	                                                
 	
+	entrada_flag : process(clk,reset)
+    begin
+        if(reset = '1') then
+            aux_pulso <= (others =>'0');
+        elsif(clk = '1' and clk'event) then
+            aux_pulso <= aux_pulso(1 downto 0)&Generacion_pieza_flag;
+        end if;
+    end process;
+    pulso_flag <= aux_pulso(1) and not(aux_pulso(2));
 end Behavioral;
