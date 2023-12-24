38b846097e7dbf702faa24c82cbcfabd76e37e09
CarlosMR18 <103529858+CarlosMR18@users.noreply.github.com>
2023-12-24 20:20:32 +0100

    Update Bloque_Aleatorio.vhdl

--- Bloque_Aleatorio.vhdl
+++ Bloque_Aleatorio.vhdl
@@ -4,40 +4,40 @@ use IEEE.NUMERIC_STD.ALL;
 -- Falta definir TOP_LFSR con Maquina de Estados
 
 entity Bloque_Aleatorio is
-Port (	clk : in STD_LOGIC;
-        reset : in STD_LOGIC;
-	  FaltaPieza_Flag : in STD_LOGIC;
-	PiezaGenerada_Flag : out STD_LOGIC;
-	PiezaAleatoria : out UNSIGNED;		   
-	--Sin Acabar
+Port (	clk : in std_logic;
+        reset : in std_logic;
+		Generacion_pieza_flag : in std_logic;
+		
+		MiPieza_TP_Nuevo : out unsigned(6 downto 0);
+		PiezaAleatoria : out unsigned;
+		--Sin Acabar
 		   );
 end Bloque_Aleatorio;
 
 
-architecture Structure of TOP_LFSR is
+architecture Structure of Bloque_Aleatorio is
     -- component
 	--signal Nombre : STD_LOGIC_VECTOR(3 downto 0) := "0000";
 
-	component LFSR is
-	Port (	clk : in STD_LOGIC;
-		reset : in STD_LOGIC;
-		lfsr_out : out STD_LOGIC);
-	end component;
-	
-	component TOP_LFSR is
-    	Port (	clk : in STD_LOGIC;
-           	reset : in STD_LOGIC;
-           	lfsr_in : in STD_LOGIC_VECTOR(4 downto 0);
-		FaltaPieza_Flag: in STD_LOGIC;
-		PiezaElegida: in STD_LOGIC;
-		PosicionElegida: in STD_LOGIC;
-		   
-		Ena_Tipo : out STD_LOGIC;
-		E_Tipo : out STD_LOGIC_VECTOR(1 downto 0);
-		E_Pos : out STD_LOGIC_VECTOR(2 downto 0);
-		--Faltan salidas E()
-		);
-	end component;
+component TOP_LFSR is
+Port (	clk : in std_logic;
+		reset : in std_logic;
+		
+		lfsr_in : in std_logic_vector(4 downto 0);
+		Generacion_pieza_flag : in std_logic;
+		
+		TipoElegido: in std_logic;
+		SimpleElegida: in std_logic;
+		DobleElegida: in std_logic;
+		CuadradaElegida: in std_logic;
+		
+		Ena_Tipo : out std_logic;
+		E_Tipo : out std_logic_vector(1 downto 0);
+		E_Pos : out std_logic_vector(2 downto 0);
+		MiPieza_TP_Nuevo : out unsigned(6 downto 0);
+		Generacion_pieza_fin_flag : out std_logic);		   
+component TOP_LFSR;
+
 	
 	component DEC3a8 is
     	port (	E : in std_logic_vector(2 downto 0);
@@ -50,13 +50,7 @@ architecture Structure of TOP_LFSR is
 		S : out std_logic_vector(3 downto 0);
 		ena: in std_logic);
 	end component;
-	
-	component DEC1a2 is
-	port (	E : in std_logic;
-		S : out std_logic_vector(1 downto 0);
-		ena: in std_logic);
-	end component;
-	
+
 
 	component AUnsigned is
 	port (	E : in std_logic_vector(10 downto 0);
@@ -116,17 +110,10 @@ begin
 					S(2)->Tipo_C_cable,
 					ena->Ena_Tipo);
 							
-	DEC_Pos_D: DEC3a8 port map (	E->E_Pos, 
+	DEC_Pos: DEC3a8 port map (	E->E_Pos, 
 					S(4 downto 0)->Pos_S_cable,
 					ena->Tipo_S_cable);
 									
-	DEC_Pos_D: DEC2a4 port map (	E->E_Pos(1 downto 0), 
-					S->Pos_D_cable,
-					ena->Tipo_D_cable);
-	
-	DEC_Pos_C: DEC1a2 port map (	E->E_Pos(0), 
-					S->Pos_C_cable,
-					ena->Tipo_C_cable);
 								
 	AUnsigned: AUnsigned port map (	E->Pos_C_cable&Pos_D_cable&Pos_S_cable,								
 					PiezaAleatoria->PiezaAleatoria);	
