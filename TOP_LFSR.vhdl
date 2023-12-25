d8e8f909ac14bfde9383cdced7c9d76c9f7fb24c
CarlosMR18 <103529858+CarlosMR18@users.noreply.github.com>
2023-12-26 00:18:52 +0100

    Update TOP_LFSR.vhdl

--- TOP_LFSR.vhdl
+++ TOP_LFSR.vhdl
@@ -31,36 +31,36 @@ architecture Behavioral of TOP_LFSR is
 		lfsr_out : out std_logic);
 	end component;
 	
-	signal aux_pulso : std_logic_vector(2 downto 0);
+	signal aux_pulso : std_logic_vector(2 downto 0) := "000";
 	signal  pulso_flag : std_logic;
 	
 	type state_t is (Espera, Generando_Tipo, Generando_Posicion);
 	signal  STATE_LFSR : state_t;
-	signal Reg_Tipo : std_logic_vector(1 downto 0);
-	signal aux_fin : std_logic;
+	signal Reg_Tipo : std_logic_vector(1 downto 0) := (others => '0');
+	signal aux_fin : std_logic := '0';
 Begin
     
-	LFSR1 : LFSR port mat  (clk -> clk,
+	LFSR1 : LFSR port map  (clk -> clk,
                             reset -> reset,
                             lfsr_out -> lfsr_in(0));
-    LFSR2 : LFSR port mat  (clk -> clk,
+    LFSR2 : LFSR port map  (clk -> clk,
                             reset -> reset,
                             lfsr_out -> lfsr_in(1));	
-    LFSR3 : LFSR port mat  (clk -> clk,
+    LFSR3 : LFSR port map  (clk -> clk,
                             reset -> reset,
                             lfsr_out -> lfsr_in(2));	
-    LFSR4 : LFSR port mat  (clk -> clk,
+    LFSR4 : LFSR port map  (clk -> clk,
                             reset -> reset,
                             lfsr_out -> lfsr_in(3));	
-    LFSR5 : LFSR port mat  (clk -> clk,
+    LFSR5 : LFSR port map  (clk -> clk,
                             reset -> reset,
                             lfsr_out -> lfsr_in(4));
 							
 	entrada_flag : process(clk,reset)
     begin
-        if(reset = '1') then
+        if (reset = '1') then
             aux_pulso <= (others =>'0');
-        elsif(clk = '1' and clk'event) then
+        elsif (clk = '1' and clk'event) then
             aux_pulso <= aux_pulso(1 downto 0)&Generacion_pieza_flag;
         end if;
     end process;
@@ -69,35 +69,37 @@ Begin
 	
     Maquina_Estados : process(clk,reset)
     begin
-        if(reset = '1') then
+        if (reset = '1') then
             STATE_LFSR <= Espera;
-        elsif(clk = '1' and clk'event) then    
+        elsif (clk = '1' and clk'event) then    
            case STATE_LFSR is
 		   
                when Espera =>
-                   if(Generacion_pieza_flag = '1') then
+                   if (Generacion_pieza_flag = '1') then
                     STATE_LFSR <= Generando_Tipo;
 					Reg_Tipo <= (others => '0');
 					aux_fin <= '0';
 					
                when Generando_Tipo =>
-                   if(TipoElegido = '1') then
+                   if (TipoElegido = '1') then
                     STATE_LFSR <= Generando_Posicion;
 					Reg_Tipo <= E_Tipo;
                     
                when Generando_Posicion =>
-                   if(E_Tipo = "00" and SimpleElegida = '1' or E_Tipo = "01" and DobleElegida = '1' or E_Tipo = "10" and CuadradaElegida = '1') then
+                   if ((E_Tipo = "00" and SimpleElegida = '1') or (E_Tipo = "01" and DobleElegida = '1') or (E_Tipo = "10" and CuadradaElegida = '1')) then
 					
 					case Reg_Tipo is
 						when "00" => 
 							if(SimpleElegida = '1') then
-								with E_Pos select
-									MiPieza_TP_Nuevo <=	"0000001" when "000",
-														"0001000" when "001",
-														"0010000" when "010",
-														"0100000" when "011",
-														"1000000" when "100",
-														"-------" when others;
+								case E_Pos is
+    when "000" => MiPieza_TP_Nuevo <= "0000001";
+    when "001" => MiPieza_TP_Nuevo <= "0001000";
+    when "010" => MiPieza_TP_Nuevo <= "0010000";
+    when "011" => MiPieza_TP_Nuevo <= "0100000";
+    when "100" => MiPieza_TP_Nuevo <= "1000000";
+    when others => MiPieza_TP_Nuevo <= (others => '0');
+end case;
+
 						when "01" => 
 							if(DobleElegida = '1') then
 								with E_Pos(1 downto 0) select
@@ -113,7 +115,7 @@ Begin
 														"1011100" when "1",
 														"-------" when others;
 						when others =>
-							--Nada
+							MiPieza_TP_Nuevo <= (others => '0');
 														
 					end case;
 					
