e3f753d1ebeb669a253d9882a8d20a0a1347bf1b
CarlosMR18 <103529858+CarlosMR18@users.noreply.github.com>
2023-12-25 13:45:59 +0100

    Update Bloque_Aleatorio.vhdl

--- Bloque_Aleatorio.vhdl
+++ Bloque_Aleatorio.vhdl
@@ -7,9 +7,9 @@ entity Bloque_Aleatorio is
 Port (	clk : in std_logic;
         reset : in std_logic;
 		Generacion_pieza_flag : in std_logic;
-		
+		MiPieza_ND_Nuevo : out unsigned(2 downto 0);
 		MiPieza_TP_Nuevo : out unsigned(6 downto 0);
-		PiezaAleatoria : out unsigned;
+		Generacion_pieza_fin_flag : out unsigned;
 		--Sin Acabar
 		   );
 end Bloque_Aleatorio;
@@ -19,24 +19,30 @@ architecture Structure of Bloque_Aleatorio is
     -- component
 	--signal Nombre : STD_LOGIC_VECTOR(3 downto 0) := "0000";
 
-component TOP_LFSR is
-Port (	clk : in std_logic;
+	component LFSR is
+	Port (	clk : in std_logic;
 		reset : in std_logic;
-		
-		lfsr_in : in std_logic_vector(4 downto 0);
-		Generacion_pieza_flag : in std_logic;
-		
-		TipoElegido: in std_logic;
-		SimpleElegida: in std_logic;
-		DobleElegida: in std_logic;
-		CuadradaElegida: in std_logic;
-		
-		Ena_Tipo : out std_logic;
-		E_Tipo : out std_logic_vector(1 downto 0);
-		E_Pos : out std_logic_vector(2 downto 0);
-		MiPieza_TP_Nuevo : out unsigned(6 downto 0);
-		Generacion_pieza_fin_flag : out std_logic);		   
-component TOP_LFSR;
+		lfsr_out : out std_logic);
+	end component;
+	
+	component TOP_LFSR is
+	Port (	clk : in std_logic;
+			reset : in std_logic;
+			
+			lfsr_in : in std_logic_vector(4 downto 0);
+			Generacion_pieza_flag : in std_logic;
+			
+			TipoElegido: in std_logic;
+			SimpleElegida: in std_logic;
+			DobleElegida: in std_logic;
+			CuadradaElegida: in std_logic;
+			
+			Ena_Tipo : out std_logic;
+			E_Tipo : out std_logic_vector(1 downto 0);
+			E_Pos : out std_logic_vector(2 downto 0);
+			MiPieza_TP_Nuevo : out unsigned(6 downto 0);
+			Generacion_pieza_fin_flag : out std_logic);		   
+	component TOP_LFSR;
 
 	
 	component DEC3a8 is
@@ -57,83 +63,79 @@ component TOP_LFSR;
 		PiezaAleatoria : out UNSIGNED;);
 	end component;
 
-	signal clk : STD_LOGIC;
-	signal reset : STD_LOGIC;
+	signal clk : std_logic;
+	signal reset : std_logic;
 	
-	signal lfsr_in_cable : STD_LOGIC_VECTOR(5 downto 0);
-	signal PiezaElegida : STD_LOGIC;
-	signal PosicionElegida : STD_LOGIC;
-	signal Ena_Tipo : STD_LOGIC;
-	signal E_Tipo : STD_LOGIC_VECTOR(1 downto 0);
-	signal E_Pos : STD_LOGIC_VECTOR(2 downto 0);
+	signal lfsr_cable : std_logic_vector(4 downto 0);
 	
-	signal Tipo_S_cable : STD_LOGIC;
-	signal Tipo_D_cable : STD_LOGIC;
-	signal Tipo_C_cable : STD_LOGIC;
+	signal TipoElegido : std_logic;
 	
-	signal Pos_S_cable: STD_LOGIC_VECTOR(4 downto 0);;
-	signal Pos_D_cable: STD_LOGIC_VECTOR(3 downto 0);;
-	signal Pos_C_cable : STD_LOGIC_VECTOR(1 downto 0);;
+	signal Tipo_S_cable : std_logic;
+	signal Tipo_D_cable : std_logic;
+	signal Tipo_C_cable : std_logic;
 	
-	--signal E_Aunsigned_cable : STD_LOGIC_VECTOR(10 downto 0);
+	signal Ena_Tipo : std_logic;
+	signal E_Tipo : std_logic_vector(1 downto 0);
+	signal E_Pos : std_logic_vector(2 downto 0);
+	
+	
+	signal Pos_S_cable: std_logic_vector(4 downto 0);
+	signal Pos_D_cable: std_logic_vector(3 downto 0);
+	signal Pos_C_cable : std_logic_vector(1 downto 0);
+	
+	signal MiPieza_TP_Nuevo: unsigned(6 downto 0);
+	signal MiPieza_ND_Nuevo_interna: unsigned(2 downto 0):="000";
+	signal Generacion_pieza_fin_flag : std_logic;
+
 		
 begin
-	LFSR1: LFSR port map (	clk->clk, 
-				reset->reset,
-				lfsr_out->lfsr_in_cable(0));						
-	LFSR2: LFSR port map (	clk->clk, 
-				reset->reset,
-				lfsr_out->lfsr_in_cable(1));
-	LFSR3: LFSR port map (	clk->clk, 
-				reset->reset,
-				lfsr_out->lfsr_in_cable(2));
-	LFSR4: LFSR port map (	clk->clk, 
-				reset->reset,
-				lfsr_out->lfsr_in_cable(3));
-	LFSR5: LFSR port map (	clk->clk, 
-				reset->reset,
-				lfsr_out->lfsr_in_cable(4));
+
+	LFSR1 : LFSR port mat  (clk -> clk,
+                            reset -> reset,
+                            lfsr_out -> lfsr_cable(0));
+    LFSR2 : LFSR port mat  (clk -> clk,
+                            reset -> reset,
+                            lfsr_out -> lfsr_cable(1));	
+    LFSR3 : LFSR port mat  (clk -> clk,
+                            reset -> reset,
+                            lfsr_out -> lfsr_cable(2));	
+    LFSR4 : LFSR port mat  (clk -> clk,
+                            reset -> reset,
+                            lfsr_out -> lfsr_cable(3));	
+    LFSR5 : LFSR port mat  (clk -> clk,
+                            reset -> reset,
+                            lfsr_out -> lfsr_cable(4));
 							
-	TOP_LFSR: TOP_LFSR port map (	clk->clk, 
-					reset->reset,
-					lfsr_in->lfsr_in_cable,
-					FaltaPieza_Flag->FaltaPieza_Flag,
-					PiezaElegida->PiezaElegida,
-					PosicionElegida->PosicionElegida,
-					Ena_Tipo->Ena_Tipo,
-					E_Tipo->E_Tipo,
-					E_Pos->E_Pos);
-									
+	TOP_LFSR: TOP_LFSR port map (	clk -> clk, 
+									reset -> reset,
+									lfsr_in -> lfsr_cable,
+									Generacion_pieza_flag -> Generacion_pieza_flag,
+									TipoElegido -> TipoElegido,
+									SimpleElegida -> Tipo_S_cable,
+	                                DobleElegida -> Tipo_D_cable,
+			                        CuadradaElegid -> Tipo_C_cable,
+			                        
+			                        Ena_Tipo -> Ena_Tipo,
+			                        E_Tipo -> E_Tipo,
+                                    E_Pos -> E_Pos,
+			                        MiPieza_TP_Nuevo -> MiPieza_TP_Nuevo,
+			                        Generacion_pieza_fin_flag -> Generacion_pieza_fin_flag);
+			
+			
+			
+	
 	DEC_Tipo: DEC2a4 port map (	E->E_Tipo, 
-					S(0)->Tipo_S_cable,
-					S(1)->Tipo_D_cable,
-					S(2)->Tipo_C_cable,
-					ena->Ena_Tipo);
+								S(2 downto 0)->Tipo_C_cable&Tipo_D_cable&Tipo_S_cable,
+								ena->Ena_Tipo);
 							
 	DEC_Pos: DEC3a8 port map (	E->E_Pos, 
-					S(4 downto 0)->Pos_S_cable,
-					ena->Tipo_S_cable);
-									
+								S->Pos_S_cable,
+								ena->TipoElegido);
 								
-	AUnsigned: AUnsigned port map (	E->Pos_C_cable&Pos_D_cable&Pos_S_cable,								
-					PiezaAleatoria->PiezaAleatoria);	
-								
-	PiezaElegida <= Tipo_S_cable or 
-			Tipo_D_cable or 
-			Tipo_C_cable;
+	TipoElegido <= Tipo_S_cable or 
+					Tipo_D_cable or 
+					Tipo_C_cable;
 
-	PosicionElegida <=	Pos_S_cable(4) or 
-				Pos_S_cable(3) or 
-				Pos_S_cable(2) or 
-				Pos_S_cable(1) or 
-				Pos_S_cable(0) or 
-				Pos_D_cable(3) or 
-				Pos_D_cable(2) or 
-				Pos_D_cable(1) or 
-				Pos_D_cable(0) or
-				Pos_C_cable(1) or 
-				Pos_C_cable(0);
-
-	PiezaGenerada_Flag <=	PiezaElegida and PosicionElegida;
-
+	MiPieza_ND_Nuevo <= MiPieza_ND_Nuevo_interna;
+	
 end Structure;
