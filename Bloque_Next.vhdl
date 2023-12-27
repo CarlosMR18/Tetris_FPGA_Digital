ea4fc944420574b9ff0c483d85254ac83ebd4f2d
CarlosMR18 <103529858+CarlosMR18@users.noreply.github.com>
2023-12-27 01:38:47 +0100

    Update Bloque_Next.vhdl

--- Bloque_Next.vhdl
+++ Bloque_Next.vhdl
@@ -122,7 +122,6 @@ architecture Behavioral of Bloque_Next is
 	
 	signal Producto_BitaBit : unsigned(6 downto 0);
 	signal Suma_BitaBit : unsigned(6 downto 0);
-	signal Ei_transformada : unsigned(6 downto 0);
 	signal E_aux : unsigned(6 downto 0);
 	signal aux_seg_E : std_logic;
 	signal aux_seg_F : std_logic;
@@ -154,25 +153,25 @@ process(clk, reset)
 				ena_time <= not(ena_time); --Señal auxiliar, duplica periodo. 
 				if(Producto_BitaBit/="0000000") then
 					case MiPieza_ND_act is
-						when "00" => (	E1_act <= Ei_transformada,
+						when "00" => (	E1_act <= E1 or MiPieza_TP,
 										E2_act <= E2,
 										E3_act <= E3,
 										E4_act <= E4);
 										
 						when "01" => (	E1_act <= E1,
-										E2_act <= Ei_transformada,
+										E2_act <= E2 or MiPieza_TP,
 										E3_act <= E3,
 										E4_act <= E4);
 										
 						when "10" => (	E1_act <= E1,
 										E2_act <= E2,
-										E3_act <= Ei_transformada,
+										E3_act <= E3 or MiPieza_TP,
 										E4_act <= E4);
 										
 						when "11" => (	E1_act <= E1,
 										E2_act <= E2,
 										E3_act <= E3,
-										E4_act <= Ei_transformada);
+										E4_act <= E4 or MiPieza_TP);
 					end case;
 				else 
 					MiPieza_ND_act <= MiPieza_ND + 1;
@@ -183,25 +182,33 @@ process(clk, reset)
 				ena_time <= '0';
 				if(Producto_BitaBit/="0000000") then
 					case MiPieza_ND_act is
-						when "00" => (	E1_act <= Ei_transformada,
+						when "000" => (	E1_act <= E1,
 										E2_act <= E2,
 										E3_act <= E3,
 										E4_act <= E4);
 										
-						when "01" => (	E1_act <= E1,
-										E2_act <= Ei_transformada,
+						when "001" => (	E1_act <= E1 or MiPieza_TP_int,
+										E2_act <= E2,
 										E3_act <= E3,
 										E4_act <= E4);
 										
-						when "10" => (	E1_act <= E1,
-										E2_act <= E2,
-										E3_act <= Ei_transformada,
+						when "010" => (	E1_act <= E1,
+										E2_act <= E2 or MiPieza_TP_int,
+										E3_act <= E3,
 										E4_act <= E4);
 										
-						when "11" => (	E1_act <= E1,
+						when "011" => (	E1_act <= E1,
+										E2_act <= E2,
+										E3_act <= E3 or MiPieza_TP_int,
+										E4_act <= E4);
+										
+						when "100" => (	E1_act <= E1,
 										E2_act <= E2,
 										E3_act <= E3,
-										E4_act <= Ei_transformada);
+										E4_act <= E4 or MiPieza_TP_int);	
+										
+						when others => ;
+						
 					end case;
 				end if;
 				ESTADO <= ESPERA;
@@ -210,25 +217,28 @@ process(clk, reset)
 				ena_time <= '0';
 				if(Producto_BitaBit/="0000000") then
 					case MiPieza_ND_act is
-						when "00" => (	E1_act <= Ei_transformada,
+						when "001" => (	E1_act <= MiPieza_ND or E1,
 										E2_act <= E2,
 										E3_act <= E3,
 										E4_act <= E4);
 										
-						when "01" => (	E1_act <= E1,
-										E2_act <= Ei_transformada,
+						when "010" => (	E1_act <= E1,
+										E2_act <= MiPieza_ND or E2,
 										E3_act <= E3,
 										E4_act <= E4);
 										
-						when "10" => (	E1_act <= E1,
+						when "011" => (	E1_act <= E1,
 										E2_act <= E2,
-										E3_act <= Ei_transformada,
+										E3_act <= MiPieza_ND or E3,
 										E4_act <= E4);
 										
-						when "11" => (	E1_act <= E1,
+						when "100" => (	E1_act <= E1,
 										E2_act <= E2,
 										E3_act <= E3,
-										E4_act <= Ei_transformada);
+										E4_act <= MiPieza_ND or E4);
+						
+						when others =>	;
+						
 					end case;
 				else
 					MiPieza_ND_act <= MiPieza_ND + 1;
@@ -275,10 +285,12 @@ E_int <=	E_aux(6 downto 3)&aux_seg_C&aux_seg_B&E_aux(0) when (MiPieza_ND = "000"
 			E4 when (MiPieza_ND = "011" and ESTADO = RAPIDO_BAJO) else
 			"1111111"; --NO ACTUA 
 
-MiPieza_TP_int <=	"0000010" when (ESTADO = RAPIDO_ALTO and MiPieza_TP(5) = '1'),
-					"0000100" when (ESTADO = RAPIDO_ALTO and MiPieza_TP(4) = '1'),
-					"0010000" when (ESTADO = RAPIDO_BAJO and MiPieza_TP(1) = '1'),
-					"0001000" when (ESTADO = RAPIDO_BAJO and MiPieza_TP(2) = '1'),
+MiPieza_TP_int <=	"0100000" when (ESTADO = RAPIDO_ALTO and MiPieza_TP(5) = '1' and MiPieza_ND = "000"),
+					"0010000" when (ESTADO = RAPIDO_ALTO and MiPieza_TP(4) = '1' and MiPieza_ND = "000"),
+					"0000010" when (ESTADO = RAPIDO_ALTO and MiPieza_TP(5) = '1' and MiPieza_ND = "000"),
+					"0000100" when (ESTADO = RAPIDO_ALTO and MiPieza_TP(4) = '1' and MiPieza_ND = "000"),
+					"0100000" when (ESTADO = RAPIDO_BAJO and MiPieza_TP(1) = '1'),
+					"0010000" when (ESTADO = RAPIDO_BAJO and MiPieza_TP(2) = '1'),
 					MiPieza_TP when others;
 
 Producto_BitaBit <= E_int and MiPieza_TP_int;
@@ -294,63 +306,3 @@ with ESTADO select
 						MiPieza_TP when others;
 
 end Behavioral;
-
-
-
-						
---------------------------------------------------------------------------------
-	localizo_Display: process (clk, reset, Next_Flag)
-	begin                                  
-		if (reset = '1') then
-			E_int <= (others => '0');
-		elsif (clk = '1' and clk'event) then
-			if (Next_Flag = '1') then 
-				with MiPieza_ND select             
-					E_int <= 	E1 when "00",      
-								E2 when "01",
-								E3 when "10",
-								E4 when "11",
-								"1111111" when others;			
-					E_int(1) <= E_int(1) or E_int(5);
-					E_int(2) <= E_int(2) or E_int(4);
-					
-			end if;
-		end if;
-	end process localizo_Display;
-	
-	Producto_BitaBit <= E_int and MiPieza_TP;
-	Suma_BitaBit <= E_int or MiPieza_TP;
-	
-	actualizo_bajada_o_fijada: process(Producto_BitaBit, PiezaFijada_Flag, PiezaBajada_Flag, MiPieza_ND_act, MiPieza_ND, E1, E2, E3, E4, Ei_transformada, E1_act, E2_act, E3_act, E4_act)
-	begin 
-		if(Producto_BitaBit = "0000000") then 	-- Accion Compatible (Baja Pieza)
-			PiezaFijada_Flag <= '1';
-			PiezaBajada_Flag <= '0';
-			MiPieza_ND_act <= MiPieza_ND + 1;
-		else 									-- Accion Incompatible (Fija Pieza)
-			PiezaFijada_Flag <= '0';
-			PiezaBajada_Flag <= '1';
-			case MiPieza_ND_act is
-				when "00" => (	E1_act <= Ei_transformada,
-								E2_act <= E2,
-								E3_act <= E3,
-								E4_act <= E4);
-								
-				when "01" => (	E1_act <= E1,
-								E2_act <= Ei_transformada,
-								E3_act <= E3,
-								E4_act <= E4);
-								
-				when "10" => (	E1_act <= E1,
-								E2_act <= E2,
-								E3_act <= Ei_transformada,
-								E4_act <= E4);
-								
-				when "11" => (	E1_act <= E1,
-								E2_act <= E2,
-								E3_act <= E3,
-								E4_act <= Ei_transformada);
-			end case;					
-		end if;
-		
-	end process actualizo_bajada_o_fijada;					
