7ae3470d2154bd8421296a3aabce1e3a75c76d0e
mila170 <152605812+mila170@users.noreply.github.com>
2023-12-30 12:44:21 +0100

    Update estructura.vhd

--- estructura.vhd
+++ estructura.vhd
@@ -36,13 +36,7 @@ entity estructura is
         clk :           in std_logic;
         reset :         in std_logic;
         
-        B_pausa :       in std_logic;
-        B_start :       in std_logic; 
-        B_reset :       in std_logic; 
-        B_m_dcha :      in std_logic;
-        B_m_izq :       in std_logic;
-        B_g_dcha :      in std_logic;
-        B_g_izq :       in std_logic;
+
         
         led :           out std_logic_vector (7 downto 0);
         segments :      out std_logic_vector (7 downto 0);
@@ -54,37 +48,31 @@ end estructura;
 architecture Behavioral of estructura is
     component Secuencia_FSM is
         port(
-             clk :          in STD_LOGIC;
-             reset :        in STD_LOGIC;
-             pausa :        in std_logic;
-             start :        in std_logic; 
-             reset_juego :  in std_logic; 
-             m_dcha :       in std_logic;
-             m_izq :        in std_logic;
-             g_dcha :       in std_logic;
-             g_izq :        in std_logic);
+             clk          :        in STD_LOGIC;
+             reset        :        in STD_LOGIC;
+             pausa        :        in std_logic;
+             start_resume :        in std_logic; 
+             reset_juego  :        in std_logic; 
+             m_dcha       :        in std_logic;
+             m_izq        :        in std_logic;
+             g_dcha       :        in std_logic;
+             g_izq        :        in std_logic);
              --faltan (o sobran) cosas, esta empezado para darnos una idea
     end component;
     
-    component Antirrebotes is
-        port(
-            boton :     in std_logic;
-            clk :       in std_logic;
-            reset :     in std_logic;
-            filtrado :  out std_logic);
-    end component;
+
     
     --faltan components
     
-    --Señales internas--
-    signal pausa :       std_logic;
-    signal start :       std_logic;
-    signal reset_juego : std_logic;
-    signal m_dcha :      std_logic;
-    signal m_izq :       std_logic;
-    signal g_dcha :      std_logic;
-    signal g_izq :       std_logic;
-    --faltan señales internas
+    --SeÃ±ales internas--
+    signal pausa :          std_logic;
+    signal start_resume :   std_logic;
+    signal reset_juego :    std_logic;
+    signal m_dcha :         std_logic;
+    signal m_izq :          std_logic;
+    signal g_dcha :         std_logic;
+    signal g_izq :          std_logic;
+    --faltan seÃ±ales internas
     
 
 begin
@@ -94,61 +82,14 @@ begin
         clk            => clk,
         reset          => reset,
         pausa          =>  pausa,
-        start          => start,
+        start_resume   => start_resume,
         reset_juego    => reset_juego, 
         m_dcha         => m_dcha,
         m_izq          => m_izq,
         g_dcha         => g_dcha,
         g_izq          => g_izq );
         
-     Anti_pausa: Antirrebotes
-     port map(
-        clk       => clk,
-        reset     => reset,
-        boton     => B_pausa,
-        filtrado  => pausa );  
-        
-     Anti_start: Antirrebotes
-     port map(
-        clk       => clk,
-        reset     => reset,
-        boton     => B_start,
-        filtrado  => start );    
-        
-     Anti_reset: Antirrebotes
-     port map(
-        clk       => clk,
-        reset     => reset,
-        boton     => B_reset,
-        filtrado  => reset_juego ); 
      
-     Anti_m_dcha: Antirrebotes
-     port map(
-        clk       => clk,
-        reset     => reset,
-        boton     => B_m_dcha,
-        filtrado  => m_dcha ); 
-
-     Anti_m_izq: Antirrebotes
-     port map(
-        clk       => clk,
-        reset     => reset,
-        boton     => B_m_izq,
-        filtrado  => m_izq );        
-
-     Anti_g_dcha: Antirrebotes
-     port map(
-        clk       => clk,
-        reset     => reset,
-        boton     => B_g_dcha,
-        filtrado  => g_dcha );
-        
-     Anti_g_izq: Antirrebotes
-     port map(
-        clk       => clk,
-        reset     => reset,
-        boton     => B_g_izq,
-        filtrado  => g_izq );
 
 
 end Behavioral;
