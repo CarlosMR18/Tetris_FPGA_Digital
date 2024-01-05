fecf53c794e03944a8a901d5da1abf5a293751a9
loreeena14 <152604303+loreeena14@users.noreply.github.com>
2024-01-05 17:37:08 +0100

    Descenso.vhd

--- Descenso.vhd
+++ Descenso.vhd
@@ -35,17 +35,18 @@ entity DescensoFichas is
 Port (clk : in std_logic;  -- Reloj del sistema
       reset : in std_logic;  -- Señal de reinicio
       MiPieza_ND : in unsigned(2 downto 0);
-     -- MiPieza_TP: in unsigned(6 downto 0);
-	 -- inicio_flag: out std_logic
+      MiPieza_TP: in unsigned(6 downto 0);
+      -- inicio_flag: out std_logic
  );
 end DescensoFichas;
 
 architecture Behavioral of DescensoFichas is
     
-	constant frec_max: integer:=125000000;
-	constant frec_new: integer:=125000000*(2-(MiPieza_ND*0.2));
-	signal cont: integer range 0 to frec_max-1;
-	signal cont_new: integer range 0 to frec_new-1;
+	--constant frec_max: integer:=125000000;
+	constant frec_2: integer:=125000000*(2-(MiPieza_ND*0.2));
+	constant frec_1: integer:=125000000*(2-(MiPieza_ND*0.1));
+	signal cont_2: integer range 0 to frec_2;
+	signal cont_1: integer range 0 to frec_1;
 	signal enable_flag: std_logic;
 	
 begin
@@ -53,15 +54,23 @@ begin
    process(clk, reset)
    begin
         if reset = '1' then
-           cont_new<=0; 
+           cont_2<=0;
+	   cont_1<=0;
         elsif (clk'event and clk = '1') then
-				if (cont_new=frec_new-1)
-					cont_new<=0;
-				else
-					cont_new<=cont_new+1;
-				end if;
-		end if;
+		if (MiPieza_TP=...)	
+			if (cont_1=frec_1-1)
+				cont_1<=0;
+			else
+				cont_1<=cont_1+1;
+			end if;
+		else
+		    	if (cont_2=frec_2-1)
+				cont_2<=0;
+			else
+				cont_2<=cont_2+1;
+			end if;
+	end if;
     end process;
 
-  enable_flag<='1' when (cont_new=cont_new-1) else '0';
+  enable_flag<='1' when ((cont_2=cont_2-1) or (cont_1=cont_1-1)) else '0';
   end Behavioral;
